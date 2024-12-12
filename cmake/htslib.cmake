7e127ad58ff63badee54d1d0ad0fc9ea29145dec
Christopher Adelmann <christopher.adelmann@gmail.com>
2024-12-12 18:27:21 +0100

    Updated htslib.cmake

--- cmake/htslib.cmake
+++ cmake/htslib.cmake
@@ -63,7 +63,7 @@ else()
     URL https://github.com/samtools/htslib/releases/download/1.20/htslib-1.20.tar.bz2
     BUILD_IN_SOURCE 1
     UPDATE_COMMAND ""
-    CONFIGURE_COMMAND echo ${LOCAL_ZLIB_CONFIG} && autoreconf -i && ./configure --prefix=${htslib_PREFIX} ${disable_flags} CXX=$ENV{CXX} CC=$ENV{CC} ${LOCAL_ZLIB_CONFIG}
+    CONFIGURE_COMMAND autoreconf -i && ./configure --prefix=${htslib_PREFIX} ${disable_flags} CXX=$ENV{CXX} CC=$ENV{CC} $ENV{LOCAL_ZLIB_CONFIG}
     BUILD_COMMAND ${MAKE_COMMAND} lib-static CXX=$ENV{CXX} CC=$ENV{CC}
     INSTALL_COMMAND ${MAKE_COMMAND} install prefix=${htslib_INSTALL}
   )
@@ -79,7 +79,7 @@ else()
     message(STATUS "ZLIB_BUILD: ${ZLIB_BUILD}")
     if(ZLIB_BUILD)
         include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/zlib.cmake)
-        set(LOCAL_ZLIB_CONFIG "CPPFLAGS=-I${CMAKE_BINARY_DIR}/submodules/zlib-install/include/  LDFLAGS=-L{CMAKE_BINARY_DIR}/submodules/zlib-install/include/")
+        set(ENV{LOCAL_ZLIB_CONFIG} "CPPFLAGS=-I${CMAKE_BINARY_DIR}/submodules/zlib-install/include/  LDFLAGS=-L{CMAKE_BINARY_DIR}/submodules/zlib-install/include/")
         message(STATUS "Updated configure command: ${LOCAL_ZLIB_CONFIG}")
         add_dependencies(htslib zlib)
     else()
@@ -91,7 +91,7 @@ else()
             # build zlib from source
             message(STATUS "Building zlib from source")
             include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/zlib.cmake)
-            set(LOCAL_ZLIB_CONFIG "CPPFLAGS=-I${CMAKE_BINARY_DIR}/submodules/zlib-install/include/  LDFLAGS=-L{CMAKE_BINARY_DIR}/submodules/zlib-install/include/")
+            set(ENV{LOCAL_ZLIB_CONFIG} "CPPFLAGS=-I${CMAKE_BINARY_DIR}/submodules/zlib-install/include/  LDFLAGS=-L{CMAKE_BINARY_DIR}/submodules/zlib-install/include/")
             message(STATUS "Updated configure command: ${LOCAL_ZLIB_CONFIG}")
             add_dependencies(htslib zlib)
             list(APPEND deps_LIB ${zlib_LIBRARIES})
