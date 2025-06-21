9d587aab0d4725009bfbc6c76c09b390945e69a7
Christopher Adelmann <christopher.adelmann@gmail.com>
2025-06-21 10:22:22 +0200

    Added logging for missing supplementary region

--- src/pipelines/analyze/ParallelInteractionClusterGenerator.cpp
+++ src/pipelines/analyze/ParallelInteractionClusterGenerator.cpp
@@ -150,10 +150,12 @@ void ParallelInteractionClusterGenerator::annotatePartiallyAnnotatedClusters(
             GenomicOrientation::fromStrandSpecificity(parameters.clusterMergingStrandSpecificity));
 
         // TODO: Fix this sometimes not working with strand specific
-        Logger::log<LogLevel::WARNING>("Could not find supplementary annotation for region: ",
-                                       segment);
-        // assert((features.size() == 1) &&
-        //        "All segments should be annotated and should have a unique feature associated");
+        if (features.empty()) {
+            Logger::log<LogLevel::WARNING>("Could not find supplementary annotation for region: ",
+                                           segment);
+        }
+        assert((features.size() == 1) &&
+               "All segments should be annotated and should have a unique feature associated");
 
         return features.front().getAnnotationID();
     };
